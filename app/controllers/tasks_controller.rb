52f2e61f586e64413df1e627138212d0ba3c1b7e
Kenta <kenta@Kentas-MacBook-Pro.local>
2026-06-18 15:33:00 +0900

    add:partial

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,8 +3,9 @@ class TasksController < ApplicationController
 
   def index
     base_date = params[:week].present? ? Date.parse(params[:week]) : Date.today
+    # 値が入っていればその日付を使い、入っていなければ今日の日付を使う
 
-    week_start = base_date.beginning_of_week(:monday)
+    week_start = base_date.beginning_of_week(:monday) # beginning_of_week = 週の開始日を返すメソッド
     week_end   = week_start + 6.days
 
     @week_days = (week_start..week_end).to_a
@@ -22,13 +23,12 @@ class TasksController < ApplicationController
 
     return redirect_to(root_path, alert: "所属クラスが設定されていません") unless current_user.classroom
 
-    @homeworks = current_user.classroom.homeworks
-                             .includes(:tasks)
-                             .where(
-                               "test_start_date <= ? AND test_end_date >= ?",
-                               @selected_date,
-                               @selected_date
-                             )
-                             .order(:test_end_date)
+    @homeworks = current_user.classroom.homeworks.includes(:tasks)
+                            .where(
+                              "test_start_date <= ? AND test_end_date >= ?",
+                              @selected_date,
+                              @selected_date
+                            )
+                            .order(:test_end_date)
   end
 end
